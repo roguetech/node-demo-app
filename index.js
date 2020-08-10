0805279727165981ee18e30d31c0026681d9f8ee
roguetech <patrick.roughan@roguetechnology.org>
2020-08-10 18:01:08 +0100

    1008

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello World! new change again!!! Another change 10082020');
+  res.send('!Hello World! new change again!!! Another change 10082020=1');
 });
 
 var server = app.listen(3000, function () {
